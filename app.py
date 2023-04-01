f7de2e823b22c6e8b92c4bb523fdb83a2ef38d98
Luuk Hofman <luhofman@deloitte.nl>
2023-04-01 10:44:22 +0200

    fixed page layout

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__, template_folder="templates")
 @app.route("/", methods=["GET", "POST"])
 def main_get(name=None):
     if request.method == "GET":
-        return render_template("main.html", name=name)
+        return render_template("main.html")
     else:
         personal_info = dict(request.form)
         bio = generate_bio(**personal_info)
-        print(bio)
+        return render_template("main.html", bio=bio)
