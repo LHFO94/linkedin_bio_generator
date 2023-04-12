2081070ba6ad155cb55df8aa1395e17b2bf1b0dd
Luuk Hofman <luhofman@deloitte.nl>
2023-04-12 22:45:20 +0200

    copy button color change now works

--- static/script.js
+++ static/script.js
@@ -1,16 +1,15 @@
-// Select the button and the spinner elements
+// get the button and the spinner elements
 const button = document.getElementById("btn_submit");
 const spinner = document.getElementById("spinner");
-const url = document.getElementById('basic_url');
+const copyIcon = document.getElementById('copy_button')
 
-// Add a click event listener to the button
+// adds a spinner to the loading button when clicked
 button.addEventListener("click", function () {
-    // button.innerText = 'Loading...'
     button.innerHTML = '<span class="spinner-grow spinner-grow-sm d-none" role="status" id="spinner" aria-hidden="true"></span>Loading...';
     document.getElementById('spinner').classList.remove('d-none');
-    //button.disabled = true
 });
 
+// form validation functionality
 document.addEventListener('input', function (event) {
     if (event.target.tagName.toLowerCase() === 'input') {
         var form = document.querySelector('form');
@@ -26,6 +25,7 @@ document.addEventListener('input', function (event) {
     }
 });
 
+// initializes the particles.js background
 window.onload = function () {
     Particles.init({
         selector: '.background',
@@ -35,8 +35,7 @@ window.onload = function () {
     });
 }
 
-// adds copy text functionality to copy_button
-copyIcon = document.querySelector('#copy_button');
+// adds copy text functionality to copy button
 copyIcon.addEventListener('click', () => {
     copyText = document.getElementById('bio_box');
     text = copyText.innerText;
@@ -50,50 +49,3 @@ copyIcon.addEventListener('click', () => {
         copyIcon.classList.remove('active')
     }
 })
-
-
-
-// function myFunction() {
-//     // Get the text field
-//     var copyText = document.getElementById("bio_box");
-
-//     // Select the text field
-//     copyText.select();
-//     copyText.setSelectionRange(0, 99999); // For mobile devices
-
-//     // Copy the text inside the text field
-//     navigator.clipboard.writeText(copyText.value);
-
-//     // Alert the copied text
-//     alert("Copied the text: " + copyText.value);
-// }
-//     output_box = document.getElementById('output_box');
-//     if (output_box) {
-//         console.log('output box found');
-//         output_box.classList.remove('show');
-//         setTimeout(function () {
-//             output_box.classList.add('show');
-//         }, 10);
-//     };
-// };
-
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     // Your code to run since DOM is loaded and ready
-//     animate_box();
-// });
-
-// function animate_box() {
-//     output_box = document.getElementById('output_box');
-//     if (output_box) {
-//         console.log('box found')
-//         output_box.classList.add('animated', 'fadeInDown');
-//         setTimeout(function () {
-//             output_box.classList.remove('fadeInDown');
-//         }, 10000)
-//     }
-//     else {
-//         console.log('box not found')
-//         //pass
-//     }
-// 
-// }
